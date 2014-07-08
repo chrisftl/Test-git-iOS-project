52130901e576cb460dd5e44c11edfe22218ddf16
Chris White <Chris@Chriss-MacBook-Pro.local>
2014-07-08 18:05:43 +0100

    added a test class

---
+++ CWTest.h
@@ -0,0 +1,13 @@
+//
+//  CWTest.h
+//  Git Tests
+//
+//  Created by Chris White on 08/07/2014.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CWTest : NSObject
+
+@end
---
+++ CWTest.m
@@ -0,0 +1,13 @@
+//
+//  CWTest.m
+//  Git Tests
+//
+//  Created by Chris White on 08/07/2014.
+//
+//
+
+#import "CWTest.h"
+
+@implementation CWTest
+
+@end
